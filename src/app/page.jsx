7a348ee9104dc8949e2367ebf16b398ee21694c9
Aminsouhail <souhailamine97@gmail.com>
2024-05-03 02:54:56 +0100

    fixes stuf

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -21,11 +21,11 @@ const Homepage = () => {
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
           {/* TITLE */}
           <h1 className="text-4xl md:text-4xl font-bold">
-          Full Stack Wizardry Shaping Tomorrow's Digital Realm
+            Full Stack Wizardry Shaping Tomorrow's Digital Realm
           </h1>
           {/* DESC */}
           <p className="md:text-xl">
-          Welcome to my digital playground, where creativity meets precision. As a full stack web developer, I blend technical expertise with an appreciation for design finesse. Explore my portfolio, a testament to my dedication to crafting polished digital experiences
+            Welcome to my digital playground, where creativity meets precision. As a full stack web developer, I blend technical expertise with an appreciation for design finesse. Explore my portfolio, a testament to my dedication to crafting polished digital experiences.
           </p>
           {/* BUTTONS */}
           <div className="w-full flex gap-4">
@@ -49,3 +49,4 @@ const Homepage = () => {
 };
 
 export default Homepage;
+
